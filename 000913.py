9de4a7c3808708d252c122817361be5969c52109
ANIRUDH KANNAN V P <anirudhkannan.v16@iiits.in>
2017-09-27 07:32:05 +0530

    Add files via upload

---
+++ 000913.py
@@ -0,0 +1,42 @@
+import sys
+for line in sys.stdin:
+    n=int(line)
+    nd=(n+1)//2
+    no=2*(nd**2)-1
+    print(3*no-6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
